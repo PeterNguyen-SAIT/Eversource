6f8857302f3cc5c7105a50fec9e68093af203c73
Peter Nguyen <peter.m.nguyen@edu.sait.ca>
2020-06-08 21:10:04 -0600

    Added thymeleaf template into register.html and registerSuccess.html. Modified RegisterController to use Get and Post requests.

--- src/main/java/com/example2/controllers/RegisterController.java
+++ src/main/java/com/example2/controllers/RegisterController.java
@@ -1,13 +1,31 @@
 package com.example2.controllers;
 
+import com.example2.model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class RegisterController {
-    @RequestMapping("/register")
-    public String register(Model model) {
+
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+
         return "register";
     }
+
+    @PostMapping("/register")
+    public String submitForm(@ModelAttribute("customer") Customer customer) {
+        System.out.println(customer);
+        return "registerSuccess";
+    }
+
 }
