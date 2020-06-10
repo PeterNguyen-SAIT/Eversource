e5e59cc1f3459b90b73d2830ad3576672be4000b
Peter Nguyen <peter.m.nguyen@edu.sait.ca>
2020-06-10 09:00:30 -0600

    RegisterController.java canuse database now.

--- src/main/java/com/example2/controllers/RegisterController.java
+++ src/main/java/com/example2/controllers/RegisterController.java
@@ -2,12 +2,11 @@ package com.example2.controllers;
 
 import com.example2.entity.UserEntity;
 import com.example2.model.Customer;
+import com.example2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,13 +14,13 @@ import java.util.List;
 @Controller
 public class RegisterController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/register")
     public String showForm(Model model) {
         UserEntity userEntity = new UserEntity();
         model.addAttribute("userEntity", userEntity);
-
-        //Customer customer = new Customer();
-        //model.addAttribute("customer", customer);
 
         return "register";
     }
@@ -29,6 +28,7 @@ public class RegisterController {
     @PostMapping("/register")
     public String submitForm(@ModelAttribute("userEntity") UserEntity userEntity) {
         System.out.println(userEntity);
+        userService.insertUser(userEntity);
         return "registerSuccess";
     }
 
