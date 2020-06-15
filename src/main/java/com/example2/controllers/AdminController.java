ba33b80966371348cf5c914bebacd055e7f4780c
HuyNguyen-SAIT <Nguyenquanhuy2000@gmail.com>
2020-06-14 20:37:28 -0600

    Validate login users

--- src/main/java/com/example2/controllers/AdminController.java
+++ src/main/java/com/example2/controllers/AdminController.java
@@ -2,17 +2,20 @@ package com.example2.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.SQLOutput;
 
 @Controller
 public class AdminController {
-    @RequestMapping("/admin")
 
+    @RequestMapping("/admin")
     public String admin(Model model) {
         return "admin";
     }
+
+
 
 
 }
