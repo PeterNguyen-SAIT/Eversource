7a89daf809588fb0fef7f9b798341d824e4ecf19
Bin Zhang <binzhangab@gmail.com>
2020-06-22 14:07:02 -0600

    delete web.xml

--- src/main/java/ca/sait/controllers/AdminController.java
+++ src/main/java/ca/sait/controllers/AdminController.java
@@ -1,11 +1,17 @@
 package ca.sait.controllers;
 
+import ca.sait.service.UsersService;
+import ca.sait.service.impl.UsersServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
+
 @Controller
 public class AdminController {
+
 
     @RequestMapping("/admin")
     public String admin(Model model) {
