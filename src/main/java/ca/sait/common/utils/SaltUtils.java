c90d69bf187a7f4b4c42d1a888eb98807e354148
Bin Zhang <binzhangab@gmail.com>
2020-07-13 14:17:48 -0600

    Add SaltUtils and TokenUtils

---
+++ src/main/java/ca/sait/common/utils/SaltUtils.java
@@ -0,0 +1,24 @@
+package ca.sait.common.utils;
+
+import java.util.Random;
+
+/**
+ * Generate salt
+ * @author Bin Zhang
+ * @create 2020-06-22 9:43 PM
+ */
+public class SaltUtils {
+    public static String getSalt(int n){
+        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxy0123456789!@#$%^&*()".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char aChar = chars[new Random().nextInt(chars.length)];
+            sb.append(aChar);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SaltUtils.getSalt(4));
+    }
+}
