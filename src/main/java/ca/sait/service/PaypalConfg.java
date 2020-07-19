bbb24cb956683fcb294a5123dab9e4faca99d495
Peter Nguyen <peter.m.nguyen@edu.sait.ca>
2020-07-19 15:17:25 -0600

    Added Paypal Application for transactions

---
+++ src/main/java/ca/sait/service/PaypalConfg.java
@@ -0,0 +1,44 @@
+package ca.sait.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.paypal.api.payments.PaymentHistory;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
+
+@Configuration
+class PaypalConfig {
+
+    @Value("${spring.datasource.paypal.client.id}")
+    private String clientId;
+    @Value("${spring.datasource.paypal.client.secret}")
+    private String clientSecret;
+    @Value("${spring.datasource.paypal.mode}")
+    private String mode;
+
+    @Bean
+    public Map<String, String> paypalSdkConfig() {
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("mode", mode);
+        return configMap;
+    }
+
+    @Bean
+    public OAuthTokenCredential oAuthTokenCredential() {
+        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+    }
+
+    @Bean
+    public APIContext apiContext() throws PayPalRESTException {
+        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+        context.setConfigurationMap(paypalSdkConfig());
+        return context;
+    }
+
+}
