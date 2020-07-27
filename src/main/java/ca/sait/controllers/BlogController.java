f06c5bb4d2181e9e7838c8c357659557cc549063
ThiPhuongAnh89 <thiphuonganh.nguyen@edu.sait.ca>
2020-07-26 21:31:15 -0600

    blog1, blog2.html

--- src/main/java/ca/sait/controllers/BlogController.java
+++ src/main/java/ca/sait/controllers/BlogController.java
@@ -22,6 +22,66 @@ public class BlogController {
             return "customer/blog";
 
     }
+    // July 24 temp controller for blog-sub pages
+    @GetMapping("/blog1")
+    public String showBlogPage1(ModelMap model, HttpSession session) {
+
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("loggedIn", " " + username);
+        model.addAttribute("usernameExist", username);
+        return "customer/blog1";
+
+    }
+
+    @GetMapping("/blog2")
+    public String showBlogPage2(ModelMap model, HttpSession session) {
+
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("loggedIn", " " + username);
+        model.addAttribute("usernameExist", username);
+        return "customer/blog2";
+
+    }
+
+    @GetMapping("/blog3")
+    public String showBlogPage3(ModelMap model, HttpSession session) {
+
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("loggedIn", " " + username);
+        model.addAttribute("usernameExist", username);
+        return "customer/blog3";
+
+    }
+
+    @GetMapping("/blog4")
+    public String showBlogPage4(ModelMap model, HttpSession session) {
+
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("loggedIn", " " + username);
+        model.addAttribute("usernameExist", username);
+        return "customer/blog4";
+
+    }
+
+    @GetMapping("/blog5")
+    public String showBlogPage5(ModelMap model, HttpSession session) {
+
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("loggedIn", " " + username);
+        model.addAttribute("usernameExist", username);
+        return "customer/blog5";
+
+    }
+
+    @GetMapping("/blog6")
+    public String showBlogPage6(ModelMap model, HttpSession session) {
+
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("loggedIn", " " + username);
+        model.addAttribute("usernameExist", username);
+        return "customer/blog6";
+
+    }
 
 
 }
